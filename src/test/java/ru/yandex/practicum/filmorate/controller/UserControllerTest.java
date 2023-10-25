3402c2cebdba42b2df1dce9fdebbe18141db4ad4
DmitryNazarov <dima-nazarov-98@list.ru>
2023-10-25 15:25:36 +0300

    TZ-9

--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -81,11 +81,11 @@ class UserControllerTest {
                 ));
     }
 
-    private String getContentFromFilm(String filename){
+    private String getContentFromFilm(String filename) {
         try {
             return Files.readString(ResourceUtils.getFile("classpath:" + filename).toPath(),
                     StandardCharsets.UTF_8);
-        }catch (IOException e){
+        } catch (IOException e) {
             return "";
         }
     }
