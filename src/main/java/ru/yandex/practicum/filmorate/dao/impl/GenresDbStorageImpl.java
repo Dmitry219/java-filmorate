3f2c05537386431cbd2a8f6b0f522ff539a041ae
DmitryNazarov <dima-nazarov-98@list.ru>
2023-11-25 22:17:51 +0300

    ТЗ-11.3 второе ревью

--- src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenresDbStorageImpl.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/impl/GenresDbStorageImpl.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
-import java.util.Optional;
 
 @Component
 public class GenresDbStorageImpl {
